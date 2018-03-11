d3b00cf114abfb2dbe918948d6e8fe9a27ab4105
Christophe Pont <christof337@hotmail.fr>
2018-03-11 21:59:32 +0100

    completing the extraction of ex3

--- src/TD1.1.c
+++ src/TD1.1.c
@@ -2,9 +2,9 @@
  ============================================================================
  Name        : TD1.1.c
  Author      : Christophe Pont
- Version     : 1.2
+ Version     : 1.3
  Copyright   : MIT License
- Description : Exercise 1 of TD1 : Getting a grip on computer limits, and rounding modes
+ Description : Exercise 1 of TD1 : Getting a grip on computer limits
  ============================================================================
  */
 
@@ -95,80 +95,12 @@ int exercise1(void) {
 	return EXIT_SUCCESS;
 }
 
-/**
- * Depending on the given rounding mode, computes the sum of the inverse of the factorial of the X first integers with MPFR, and print them.
- */
-int computeSumInverseFact(mpfr_rnd_t roundingMode) {
-	// declarations
-	mpfr_t s, t, u;
-	// initializations
-	mpfr_inits2(PRECISION, t, s, u, (mpfr_ptr) NULL);
-	mpfr_set_d(t, 1.0, roundingMode);
-	mpfr_set_d(s, 1.0, roundingMode);
-
-	for (unsigned int i = 1; i <= 200; ++i) {
-		mpfr_mul_ui(t, t, i, roundingMode);
-		mpfr_set_d(u, 1.0, roundingMode);
-		mpfr_div(u, u, t, roundingMode);
-		mpfr_add(s, s, u, roundingMode);
-	}
-
-	mpfr_out_str(stdout, 10, 0, s, roundingMode);
-	putchar('\n');
-	mpfr_clears(s, t, u, (mpfr_ptr) NULL);
-
-	return EXIT_SUCCESS;
-}
-
-/**
- * Compute the sum of the inverse of the factorial of the X first integers in double precision.
- */
-int computeSumInverseFactDouble(void) {
-	float sumDouble = 1; 				// 171! is Inf
-
-	for (unsigned int i = 1; i <= 200; ++i) {
-		sumDouble += 1 / factF(i);
-	}
-
-	printf("\nDouble sum is : %f", sumDouble);
-
-	return EXIT_SUCCESS;
-}
-
-/**
- * Computes the SumInverseFact for each rounding mode given by MPFR.
- */
-int exercise2(void) {
-	//	MPFR_RNDN=0,  /* round to nearest, with ties to even */
-	//  MPFR_RNDZ,    /* round toward zero */
-	//  MPFR_RNDU,    /* round toward +Inf */
-	//  MPFR_RNDD,    /* round toward -Inf */
-	//  MPFR_RNDA,    /* round away from zero */
-	//  MPFR_RNDF,    /* faithful rounding (not implemented yet) */
-	//  MPFR_RNDNA=-1 /* round to nearest, with ties away from zero (mpfr_round) */
-	printf("Round to nearest, with ties to even\t");
-	computeSumInverseFact(MPFR_RNDN);
-	printf("Round toward zero\t\t\t");
-	computeSumInverseFact(MPFR_RNDZ);
-	printf("Round toward +Inf\t\t\t");
-	computeSumInverseFact(MPFR_RNDU);
-	printf("Round toward -Inf\t\t\t");
-	computeSumInverseFact(MPFR_RNDD);
-	printf("Round away from zero\t\t\t");
-	computeSumInverseFact(MPFR_RNDA);
-
-	return EXIT_SUCCESS;
-}
-
 int main(void) {
 	int out = EXIT_FAILURE;
 
 	// ex 1
 	printf("\nExercise 1:\n");
 	out = exercise1();
-	// ex 2
-	printf("\nExercise 2:\n");
-	out += exercise2();
 
 	return out;
 }
